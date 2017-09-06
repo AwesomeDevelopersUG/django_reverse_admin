a68cd5241984f7947b56b367c1c58f85ff82b8bc
Stepan Bokoc <stepan.bokoc@awesomedevelopers.eu>
2017-09-06 15:20:57 +0200

    Increased version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-reverse-admin',
-    version='0.0.1',
+    version='0.0.2',
     packages=['django_reverse_admin',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
